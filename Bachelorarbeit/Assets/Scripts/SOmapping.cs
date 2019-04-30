f355cc29aa6279cd3d317d8f8a762acc5e559f5d
Cranson93 <hillatong@hotmail.com>
2019-04-30 20:24:29 +0200

    add somapping

---
+++ Bachelorarbeit/Assets/Scripts/SOmapping.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SOmapping : MonoBehaviour
+{
+    public Product product;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
