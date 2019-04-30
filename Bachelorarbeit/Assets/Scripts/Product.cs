feb1212fd3451d986c3bb459eeb4981309202818
Cranson93 <hillatong@hotmail.com>
2019-04-30 19:32:20 +0200

    add scriptableObjects

---
+++ Bachelorarbeit/Assets/Scripts/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Product", menuName = "Produkt")]
+public class Product : ScriptableObject
+{
+    
+    public string nameProd;
+    public string ablaufdatum;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Bachelorarbeit/Assets/Scripts/ProductDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProductDisplay : MonoBehaviour
+{
+    public Product product;
+    public Text txt_product; 
+
+   public void Start()
+    {
+        txt_product.text = product.nameProd;
+
+    }
+}
