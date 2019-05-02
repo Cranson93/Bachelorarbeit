e937580755f55b3dfb51bf7a0ee40b14c1d46517
Cranson93 <hillatong@hotmail.com>
2019-05-02 13:02:53 +0200

    add cubes and grab working

---
+++ Bachelorarbeit/Assets/Scripts/FadeInText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInText : MonoBehaviour
+{
+    public GameObject Cube;
+    // Start is called before the first frame update
+    private void OnTriggerEnter()
+    {
+      
+        Instantiate(Cube);
+    }
+
+}
