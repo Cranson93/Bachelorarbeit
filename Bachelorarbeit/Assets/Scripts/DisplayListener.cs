6f10b0b6242273ee8b1443d91d5bdb96a9b48956
Cranson93 <hillatong@hotmail.com>
2019-05-16 20:33:05 +0200

    add gaze cube first version

---
+++ Bachelorarbeit/Assets/Scripts/DisplayListener.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayListener : MonoBehaviour
+{
+    [SerializeField] private GameObject display;
+
+    private void MyPointerEnter(Camera gaze)
+    {
+        if (gaze.gameObject.CompareTag("Player"))
+        {
+            display.SetActive(true);
+            //   productInfo.gameObject.SetActive(true);
+        }
+
+
+        void MyPointerLeave()
+        {
+            if (gaze.gameObject.CompareTag("Player"))
+            {
+                display.SetActive(false);
+                //    productInfo.gameObject.SetActive(false);
+            }
+        }
+    }
+}
