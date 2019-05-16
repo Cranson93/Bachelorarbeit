d6190ce98180846e9cde868de1dc60a805fad24b
Cranson93 <hillatong@hotmail.com>
2019-05-16 19:29:33 +0200

    add working on raycast

---
+++ Bachelorarbeit/Assets/Scripts/VisionRaycast.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR.InteractionSystem;
+
+public class VisionRaycast : MonoBehaviour {
+
+    public Collider player; // holds a reference to the player camera
+
+    void Start () {
+	    
+    }
+	
+    void Update () {
+        RaycastHit hit = new RaycastHit(); // this object will collect data about the collision each frame
+
+        if (Physics.Raycast(player.transform.position, player.transform.forward, out hit)) // do the raycast, based on the camera's position and orientation, and store the hit for our reference
+        {
+            Debug.Log("We are hitting something!"); // the raycast collided with an object
+        }
+        else // there wasn't any collision
+        {
+            Debug.Log("Not hitting anything.");
+        }
+    }
+}
+
