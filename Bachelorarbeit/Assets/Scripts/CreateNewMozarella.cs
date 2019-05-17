28641cadbc4ec6d225267fc9b7d3e9ff407ef5e3
Cranson93 <hillatong@hotmail.com>
2019-05-17 21:56:36 +0200

    add almost finished

---
+++ Bachelorarbeit/Assets/Scripts/CreateNewMozarella.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateNewMozarella : MonoBehaviour
+{
+
+    [SerializeField] private GameObject food;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+           Instantiate(food); 
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
